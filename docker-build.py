b77c201b5d4b6bd8c85c4190ffc6e9ef5254eac7
Ryan Williams <ryan@runsascoded.com>
2022-07-02 14:12:33 -0400

    docker-build improvements

--- docker-build.py
+++ docker-build.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
+from glob import glob
+import re
+
 parser = ArgumentParser()
-parser.add_argument('args',nargs='*',help='Positional arguments')
-parser.add_argument('-t','--tag',help='Tag to tag image with')
+parser.add_argument('args', nargs='*', help='Positional arguments')
+parser.add_argument('-t', '--tag', help='Tag to tag image with')
 args, passthrough = parser.parse_known_args()
 
+from os import getcwd
+from os.path import basename, exists, isdir, isfile
 from subprocess import check_call as run
 
-from pathlib import Path
-
-files = [ arg for arg in args.args if Path(arg).is_file ]
-dirs = [ arg for arg in args.args if Path(arg).is_dir ]
+files = [arg for arg in args.args if isfile(arg) and exists(arg)]
+dirs = [arg for arg in args.args if isdir(arg) and exists(arg)]
 
 if files:
     if len(files) == 1:
         [dockerfile] = files
     else:
-        raise ValueError(f'Found multiple files: {files}')
+        raise ValueError(f'Found {len(files)} files: {files}')
 else:
     dockerfile = 'Dockerfile'
+    if not exists(dockerfile):
+        dockerfiles = list(glob('*.dockerfile'))
+        if len(dockerfiles) == 1:
+            [dockerfile] = dockerfiles
+        else:
+            raise RuntimeError(f"Couldn't find Dockerfile or single *.dockerfile ({dockerfiles})")
+
 
 if dirs:
     if len(dirs) == 1:
@@ -32,11 +42,15 @@ else:
 if args.tag:
     tag = args.tag
 else:
-    tag = Path.cwd().name
-    pieces = dockerfile.rsplit('.',1)
-    if len(pieces) > 1:
-       tag = f'{tag}_{pieces[1]}'
+    m = re.fullmatch(r'(?P<name>.*)\.dockerfile', basename(dockerfile))
+    if m:
+        tag = m['name']
+    else:
+        tag = basename(getcwd())
+        pieces = dockerfile.rsplit('.', 1)
+        if len(pieces) > 1:
+            tag = f'{tag}_{pieces[1]}'
 
-cmd = [ 'docker','build','-f',dockerfile,'-t',tag ] + passthrough + [ context ]
+cmd = ['docker', 'build', '-f', dockerfile, '-t', tag] + passthrough + [context]
 print(f'Running: {cmd}')
 run(cmd)
